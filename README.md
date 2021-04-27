5d016fc1f9c625a113bdbf62d2d07ee956b5d8a6
Dariuszewski <>
2021-04-27 09:09:01 +0200

    26.04.2021

--- README.md
+++ README.md
@@ -21,10 +21,17 @@
 
 3. extracion of single opinion components
 4. transformation of extracted data to given data types
-<<<<<<< HEAD
 
-## Stage 2 - extraction of all opinions for single page
+
+## Stage 2 - extraction of all opinions from single page
+1. definition of dictionary to store components of single opinion
+2. definition of list for opinions' dictionaries storing
+3. implementation of loop traversing over all opinions from single page
 
 ## Stage 3 - extraction of all opinions of product
-=======
->>>>>>> 60315514ec55a899898c6f84ae5e0f5a5d1f9a30
+1. implementation of loop traversing over consecutive pages with opinions
+2. loading extracted opinions to .json file
+3. parametrization of product id and reading product id form standard input
+
+## Stage 4 - code refactoring
+
--- main.py
+++ main.py
@@ -2,27 +2,48 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
+# get component(opinion, selector, None, True)
+# get component(opinion, selector, list=True)
+def get_component(opinion, selector, attribute=None, list=False):
+    try:
+        if attribute:
+            return opinion.select(selector).pop(0)[attribute].strip()
+        return opinion.select(selector).pop(0)[attribute].strip()   
+    except IndexError:
+        component = None
 
 all_opinions = []
 page = 1
-
+#product_id="91715703"
+product_id=input("Enter product ID: ")
+single_opinion = {
+    "opinion_id": [],
+    "author": ["span.user-post__author-name"],
+    "recommendation": recommendation,
+    "stars": stars,
+    "content": content,
+    "pros": pros,
+    "cons": cons,
+    "verfied": verfied,
+    "post_date": post_date,
+    "purchase_date": purchase_date,
+    "usefulness": usefulness,
+    "uselessness": uselessness
+}
 while True:
     print(page)
-    respons = requests.get(f"https://www.ceneo.pl/91715703/opinie-{page}", allow_redirects=False)
+    respons = requests.get(f"https://www.ceneo.pl/{product_id}/opinie-{page}", allow_redirects=False)
     if respons.status_code==200:
         page_dom = BeautifulSoup(respons.text, 'html.parser')
         opinions = page_dom.select("div.js_product-review")
         for opinion in opinions:
             opinion_id = opinion["data-entry-id"]
-            author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
-            try: 
-                recommendation = opinion.select(
-                "span.user-post__author-recomendation > em").pop(0).get_text().strip()
-                recommendation = True if recommendation=="Polecam" else False
-            except IndexError:
-                recommendation = None
-            stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
+            author = get_component(opinion, "span.user-post__author-name")
+            recommendation = get_component(opinion, "span.user-post__author-recomendation > em")
+
+            stars = get_component(opinion, "span.user-post__score-count")
             stars = float(stars.split("/")[0].replace(",","."))
+
             content = opinion.select("div.user-post__text").pop(0).get_text().strip()
             content = re.sub("\\s"," ",content)
             pros = opinion.select("div.review-feature__col:has(> div[class$=\"positives\"]) > div.review-feature__item")
@@ -43,22 +64,12 @@ while True:
             usefulness = int(opinion.select("span[id^='votes-yes']").pop(0).get_text().strip())
             uselessness = int(opinion.select("span[id^='votes-no']").pop(0).get_text().strip())
 
-            single_opinion = {
-                "opinion_id": opinion_id,
-                "author": author,
-                "recommendation": recommendation,
-                "stars": stars,
-                "content": content,
-                "pros": pros,
-                "cons": cons,
-                "verfied": verfied,
-                "post_date": post_date,
-                "purchase_date": purchase_date,
-                "usefulness": usefulness,
-                "uselessness": uselessness
-            }
+
             all_opinions.append(single_opinion)
         page += 1
     else: break
 
-# print(json.dumps(all_opinions, ensure_ascii=False, indent=4))
+with open(f"./opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, ensure_ascii=False, indent=4)
+
+print(json.dumps(all_opinions, ensure_ascii=False, indent=4))
